fbdcbd75d0adafb36477f3eccdf83e254c008795
OthiDaniel <othidaniel@gmail.com>
2019-03-26 18:27:04 +0300

    Auto-generated commit

--- src/app/app-highlight.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AppHighlightDirective } from './app-highlight.directive';
-
-describe('AppHighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AppHighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/count-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CountPipePipe } from './count-pipe.pipe';
-
-describe('CountPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CountPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
